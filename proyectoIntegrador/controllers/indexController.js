88a6a1d20689a51d8d238f7d0ecf1999e237e04a
manuelchopourian <manuelchopourian@Manuels-MacBook-Air.local>
2021-05-26 22:10:49 -0300

    search enlazado

--- proyectoIntegrador/controllers/indexController.js
+++ proyectoIntegrador/controllers/indexController.js
@@ -1,5 +1,7 @@
-let  producto = require("../data/products");
+// let  producto = require("../data/products");
+
 const db = require('../database/models');
+const op = db.Sequelize.Op;
 
 let indexController = {
 
@@ -14,19 +16,30 @@ let indexController = {
         })
       },
     
-    search: function (req,res, next){
+  //   search: function (req,res, next){
 
-      let result = []
-      for(let i=0; i < producto.length; i++) { 
+  //     let result = []
+  //     for(let i=0; i < producto.length; i++) { 
 
-        if(producto[i].producto.includes(req.query.search.toLowerCase())){
-          result.push(producto[i])
-        }
-      };
-        res.render('search-results', {
-          productos: result
+  //       if(producto[i].producto.includes(req.query.search.toLowerCase())){
+  //         result.push(producto[i])
+  //       }
+  //     };
+  //       res.render('search-results', {
+  //         productos: result
+  //   })
+  //  },
+
+  search: (req, res)=>{
+    db.Product.findAll({
+        where: [
+            { producto: {[op.like]: `%${req.query.search}%`}}
+        ]
     })
-   },
+        .then(productos => res.render('search-results', { productos }))
+        .catch(err=> console.log(err))
+    
+},
 
     login : (req, res) => { 
       res.render('login'); 
