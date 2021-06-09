c205c36641eb863e02684dfbb82032cf91ac3d30
Juan Z <juanzimmermann308@gmail.com>
2021-06-09 20:06:38 -0300

    session

--- proyectoIntegrador/app.js
+++ proyectoIntegrador/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session =require('express-session')
 
 var indexRouter = require('./routes/index');
 var productRouter = require('./routes/product');
@@ -21,6 +22,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: "nuestro mensaje secreto",
+  resave: false,
+  saveUninitialized: true}));
 
 app.use('/', indexRouter);
 app.use('/product', productRouter);
